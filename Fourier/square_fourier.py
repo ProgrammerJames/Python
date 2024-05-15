5e82ffb45201ae6ab87a4319e1116103b59534d1
James Thomas Pritchard <ProgrammerJames@users.noreply.github.com>
2024-05-15 21:52:12 +1000

    Update square_fourier.py
    Rewrote the program structure for unit testing a chosen transform wave function.
    
    This will allow for more tests to be added to ensure more comprehensive validation.

--- Fourier/square_fourier.py
+++ Fourier/square_fourier.py
@@ -1,53 +1,69 @@
 import math
 
-INPUT : list[float] = [1.0, 1.0, 1.0, 1.0, -1.0, -1.0, -1.0, -1.0]
-OUTPUT : list[float] = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-
 Pair = tuple[float, int]
 
-FList : list[Pair] = [
-	(0.0, 0),
-	(0.0, 1),
-	(0.0, 2),
-	(0.0, 3),
-	(0.0, 4),
-	(0.0, 5),
-	(0.0, 6),
-	(0.0, 7),
-]
-
-# Square wave function
+# Square wave function (range -1..1)
 def square(x : float) -> float:
 	return math.copysign(1.0, math.cos(x*math.pi))
 
-# Calculate fourier magnitudes
-for i, p in enumerate(FList):
-	#print("Frequency: ", p[1])
-	sum : float = 0.0
+# Square wave function using bit shifting (range: 0..1)
+def square_b(x : int, mx : int) -> float:
+	return (x >> mx) & 1
+
+# Calculate fourier series
+def build_fourier_series(input : list[float]) -> list[Pair]:
+	f_list : list[Pair] = [(0.0, 0)]*(len(input))
+	count : float = float(len(input)-1)
 	
-	for j, v in enumerate(INPUT):
-		val : float = square((float(j)/7.0)*p[1])
-		sum += val*v
-		#print(j, ": ", val)
+	for i, p in enumerate(f_list):
+		#print("Frequency: ", p[1])
+		sum : float = 0.0
+		
+		for j, v in enumerate(input):
+			val : float = square((float(j)/count)*i)
+			sum += val*v
+			#print(j, ": ", val)
+			pass
+		
+		sum = sum/len(input)
+		f_list[i] = (sum, i)
+		#print("Sum: ", sum)
+		#print("\n")
+		pass
+	return f_list
+
+# Calculate fourier output
+def calculate_result(f_list : list[Pair]) -> list[float]:
+	output : list[float] = [0.0]*(len(f_list))
+	count : float = float(len(f_list)-1)
+	
+	for j, v in enumerate(output):
+		for i, p in enumerate(f_list):
+			output[j] += square((float(j)/count)*p[1])*p[0]
 		pass
 	
-	sum = sum/len(INPUT)
-	FList[i] = (sum, p[1])
-	#print("Sum: ", sum)
-	#print("\n")
-	pass
+	return output
 
-# Calculate fourier series results
-for j, v in enumerate(OUTPUT):
-	for i, p in enumerate(FList):
-		OUTPUT[j] += square((float(j)/7.0)*p[1])*p[0]
-		pass
-	pass
+# Perform transformation test for input
+def test_fourier(input : list[float]):
+	# Calculate fourier magnitudes
+	f_list = build_fourier_series(input)
+	
+	# Calculate fourier series results
+	output = calculate_result(f_list)
+	
+	# Print results
+	print("")
+	print("-----  INPUT")
+	print(input)
+	print("\n-----  Square Wave Fourier Values (Magnitude, Frequency)")
+	print(f_list)
+	print("\n-----  OUTPUT (To test transform works correctly)")
+	print(output)
+	print("")
+	
+	assert input == output
 
-# Print results
-print("-----  INPUT")
-print(INPUT)
-print("\n-----  Square Wave Fourier Values (Magnitude, Frequency)")
-print(FList)
-print("\n-----  OUTPUT (To test transform works correctly)")
-print(OUTPUT)
+
+# Test Case
+test_fourier([1.0, 1.0, 1.0, 1.0, -1.0, -1.0, -1.0, -1.0])
